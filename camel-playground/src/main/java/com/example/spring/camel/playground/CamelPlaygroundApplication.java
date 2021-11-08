e699f1da22fcd508bf8b7f79327a701f933be39f
João Luzio <joao.luzio@gmail.com>
2021-11-08 02:28:45 +0000

    Playground initial

---
+++ camel-playground/src/main/java/com/example/spring/camel/playground/CamelPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.camel.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelPlaygroundApplication.class, args);
+	}
+
+}
